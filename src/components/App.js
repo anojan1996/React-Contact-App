ec65c6ba1453dd271d495e03c57631c485c7f29b
anojan1996 <anojan2012@gmail.com>
2021-09-27 14:15:09 +0530

    AddContact and ContactList Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
+import AddContact from './AddContact';
 import './App.css';
+import ContactList from './ContactList';
 import Header from "./Header";
 
 function App() {
   return (
    <div className="ui container"> 
      <Header/>
+     <AddContact/>
+     <ContactList/>
    </div>
   );
 }
