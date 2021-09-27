74ad1d9130c874bf46c9569f3bcca6c79d5c04be
anojan1996 <anojan2012@gmail.com>
2021-09-27 14:43:29 +0530

    Lists and Props

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,23 @@ import ContactList from './ContactList';
 import Header from "./Header";
 
 function App() {
+  const contacts = [
+    {
+      id:"1",
+      name:"Anojan",
+      email:"anojan2012@gmail.com" ,
+    },
+    {
+      id:"2",
+      name:"React",
+      email:"react2012@gmail.com" ,
+    },
+  ];
   return (
    <div className="ui container"> 
      <Header/>
      <AddContact/>
-     <ContactList/>
+     <ContactList contacts={contacts}/>
    </div>
   );
 }
